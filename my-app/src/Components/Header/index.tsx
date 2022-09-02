32dd0cb39fca720efd6807b8a20e1d5c0438032f
Enrickyb <enrickyb@hotmail.com>
2022-09-02 10:17:59 -0400

    install libs

---
+++ my-app/src/Components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as Styled from "./style";
+
+export const Header = () => {
+  return (
+    <Styled.Header>
+      <h1>All In One</h1>
+      <p>Todas as ferramentas em um único site</p>
+    </Styled.Header>
+  );
+};
---
+++ my-app/src/Components/Header/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+  padding: 20px;
+  background-color: white;
+  
+`;
